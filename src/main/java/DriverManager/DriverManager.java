eba53915b83f7345c98ac0969a79ec81581b92db
RahulPatankar <rahulpatankar2298@gmail.com>
2023-07-28 05:14:28 -0700

    Initial commit

---
+++ src/main/java/DriverManager/DriverManager.java
@@ -0,0 +1,20 @@
+package DriverManager;
+
+import org.openqa.selenium.WebDriver;
+
+public final class DriverManager {
+
+	private DriverManager() {
+	};
+
+//	public static ThreadLocal<WebDriver> dr = new ThreadLocal<>();
+	private final static ThreadLocal<WebDriver> dr = new ThreadLocal<WebDriver>();
+
+	public static void setDriver(WebDriver driver) {
+		dr.set(driver); // thread
+	}
+
+	public static WebDriver getDriver() {
+		return dr.get();
+	}
+}
---
+++ src/main/java/com/cs/utils/ScreenshotUtils.java
@@ -0,0 +1,17 @@
+package com.cs.utils;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import DriverManager.DriverManager;
+
+
+public final class ScreenshotUtils {
+
+	private ScreenshotUtils() {}
+	
+	public static String getScreenShot()
+	{
+		return ( (TakesScreenshot) DriverManager.getDriver()).getScreenshotAs(OutputType.BASE64);
+	}
+}
